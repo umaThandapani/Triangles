974a007c6cdab7c1ba976ab98bd6d5f39a2a7415
umathan <umathan13@gmail.com>
2015-07-16 09:11:35 -0400

    Testing on Types of Triangles
    Unit testing on three types of triangle - Equilateral, Isosceles, and
    Scalene

---
+++ TriangleTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangles
+{
+    public class TriangleTypes
+    {
+        public string DoAnotherThing(int side1, int side2, int side3)
+        {
+            string result = "";
+            if (side1 == side2 && side2 == side3)
+                result = "Equilateral";
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+                result = "Isosceles";
+            else
+                result = "Scalene";
+            return result;
+        }
+
+        
+    }
+}
